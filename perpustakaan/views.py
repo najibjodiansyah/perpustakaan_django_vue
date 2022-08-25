755202632aa5588fe5256110967bc2ca4e664dcb
Najib Jodiansyah <muhammad.najibuddin@sirclo.com>
2022-08-25 23:35:23 +0700

    feat add extends html

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def buku(request):
     return render(request,'buku.html')
+
+def penerbit(request):
+    return render(request,'penerbit.html')
