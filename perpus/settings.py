85f1492f3da176a2d5b40e1bb9003bc108f51131
Najib Jodiansyah <muhammad.najibuddin@sirclo.com>
2022-08-26 00:27:56 +0700

    set up postgresql

--- perpus/settings.py
+++ perpus/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'perpus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'perpustakaan_django',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
