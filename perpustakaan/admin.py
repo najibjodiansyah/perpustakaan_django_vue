1a7fa1294b9fb050db5345b4d9ede30d2f62a804
Najib Jodiansyah <muhammad.najibuddin@sirclo.com>
2022-08-26 00:57:23 +0700

    custom data buku admin page

--- perpustakaan/admin.py
+++ perpustakaan/admin.py
@@ -1,7 +1,15 @@
+from typing import List
 from django.contrib import admin
 from .models import Kelompok, Buku
 
 # Register your models here.
 
-admin.site.register(Kelompok)
+class BukuAdmin(admin.ModelAdmin):
+    list_display: = ['judul', 'pengarang', 'penerbit', 'tahun_terbit', 'jumlah_halaman', 'jumlah_buku', 'kategori', 'kelompok_id']
+    search_fields = ['judul', 'pengarang', 'penerbit', 'tahun_terbit', 'jumlah_halaman', 'jumlah_buku', 'kategori', 'kelompok_id']
+    list_filter = ('kelompok_id')
+    list_per_page: int = 5
+    
+
+admin.site.register(Kelompok, BukuAdmin)
 admin.site.register(Buku)
