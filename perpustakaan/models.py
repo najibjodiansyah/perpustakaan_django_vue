6de6f528f4d7fa01fa49da6834b1fa88160a7c17
Najib Jodiansyah <muhammad.najibuddin@sirclo.com>
2022-08-26 00:37:45 +0700

    add buku and kelompok models

--- perpustakaan/models.py
+++ perpustakaan/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Kelompok(models.Model):
+    nama = models.CharField(max_length=100)
+    keterangan = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_kelas
+class Buku(models.Model):
+    judul = models.CharField(max_length=100)
+    pengarang = models.CharField(max_length=100)
+    penerbit = models.CharField(max_length=100)
+    tahun_terbit = models.IntegerField()
+    jumlah_halaman = models.IntegerField()
+    jumlah_buku = models.IntegerField()
+    kategori = models.CharField(max_length=100)
+    kelompok_id = models.ForeignKey(Kelompok, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.judul
+
