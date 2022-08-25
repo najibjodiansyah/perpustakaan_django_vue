9bef5a539378dbc28fc96c6ea5f165b46536c5e3
Najib Jodiansyah <muhammad.najibuddin@sirclo.com>
2022-08-25 23:22:54 +0700

    feat add templates

--- perpus/urls.py
+++ perpus/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from perpustakaan.views import buku
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('buku/', buku)
 ]
